a4795dabca0dae9122c084e26df5fc87f132f657
Ivan Turasov <turasov.ivan@gmail.com>
2019-11-20 15:59:39 +0100

    Add message queue data types to common.h

--- interprocess/common.h
+++ interprocess/common.h
@@ -9,11 +9,25 @@
 #ifndef COMMON_H
 #define COMMON_H
 
+#include "settings.h"
+
 // maximum size for any message in the tests
 #define MAX_MESSAGE_LENGTH	6
  
-// TODO: put your definitions of the datastructures here
+// A data structure with 3 members, represents a request to a worker
+typedef struct
+{
+    uint128_t hash;
+    uint16_t hash_sequence_num;
+    char assigned_letter;
+} MQ_REQUEST_MESSAGE;
 
+// A data structure with 3 members, represents a response from a worker
+typedef struct
+{
+    uint16_t hash_sequence_num;
+    bool is_found;
+} MQ_RESPONSE_MESSAGE;
 
 #endif
 
