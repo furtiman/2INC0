dbd8c9dbff8b18d0915e590c873692d618088243
Student <r.j.a.meijer@student.tue.nl>
2019-12-13 18:17:01 +0100

    fixed bug, but still slow

--- threading/flip.c
+++ threading/flip.c
@@ -22,21 +22,18 @@
 #include "flip.h"
 
 
-void create_mask(uint128_t mask[], uint16_t flip_number);
+void create_mask(uint128_t mask[], uint32_t flip_number);
 static void* flip_thread(void* flip_number);
 static void control_threads(void);
 
 static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 int main(void) {
-    
     /* start threads, flip pieces, stop threads */
     control_threads();
 
-   //uint64_t low[(NROF_PIECES/128) + 1], high[(NROF_PIECES/128) + 1];
-
-    for (int16_t i = 0; i <= NROF_PIECES/128; i++) {
-        for (int16_t j = 0; j < 127; j++) {
+    for (uint16_t i = 0; i <= NROF_PIECES/128; i++) {
+        for (uint32_t j = 0; j < 127; j++) {
             if (buffer[i] & 1) {
                 printf("%d", i*128 + j);
                 printf("\n");
@@ -47,7 +44,7 @@ int main(void) {
     return (0);
 }
 
-void create_mask(uint128_t mask[], uint16_t flip_number) {
+void create_mask(uint128_t mask[], uint32_t flip_number) {
     const uint16_t buffer_size = NROF_PIECES/128 + 1;
 
     /* inintialize mask array*/
@@ -55,11 +52,11 @@ void create_mask(uint128_t mask[], uint16_t flip_number) {
         mask[i] = 0;
     }
 
-    for (uint16_t i = NROF_PIECES; i > 0; i--) {
+    for (uint32_t i = NROF_PIECES; i > 0; i--) {
         /* i counts backwards, piece_number counts upwards */
-        uint16_t piece_number = i - 1;
+        uint32_t piece_number = i - 1;
         /* becomes 1 if piece_number i-1 should be flipperd, 0 if piece_number i-1 should not be flipped */
-        uint16_t one_or_zero = !(piece_number % flip_number) && piece_number ? 1 : 0; 
+        bool one_or_zero = !(piece_number % flip_number) && piece_number ? 1 : 0; 
 
          for (uint16_t j = buffer_size - 1; j > 0; j--) {
              /* shifts all bits left by 1 and puts the msb of arr[i - 1] in the lsb of arr[i] */
@@ -72,37 +69,37 @@ void create_mask(uint128_t mask[], uint16_t flip_number) {
 }
 
 static void* flip_thread(void* flip_number) {
-    int *arg_number;
-    uint16_t number;
+    uint32_t *arg_number;
+    uint32_t number;
 
-    arg_number = (int*)flip_number;
+    arg_number = (uint32_t*)flip_number;
     number = *arg_number;
     free(flip_number);
 
     uint128_t mask[(NROF_PIECES/128) + 1];
     create_mask(mask, number);
 
-    pthread_mutex_lock(&mutex);
     //printf("\nlocked\n");
 
-    for (uint128_t i = 0; i <= NROF_PIECES/128; i++) {
+    pthread_mutex_lock(&mutex);
+    for (uint16_t i = 0; i <= NROF_PIECES/128; i++) {
         /* buffer XOR mask works like flipping the pieces */
         buffer[i] ^= mask[i];
     }
+    pthread_mutex_unlock(&mutex);
 
     //printf("\nunlock\n");
-    pthread_mutex_unlock(&mutex);
     return (NULL);
 }
 
 static void control_threads(void) {
-    int16_t* flip_number;
+    uint32_t* flip_number;
     /* array of threads */
     pthread_t flippers[NROF_THREADS];
 
-    for (pthread_t i = 1; i < NROF_PIECES; i++) {
-        flip_number = malloc(sizeof(int16_t));
-        *flip_number = (int16_t)i;
+    for (uint32_t i = 1; i <= NROF_PIECES; i++) {
+        flip_number = malloc(sizeof(uint32_t));
+        *flip_number = i;
 
         /* created NROF_THREADS threads and gives them the current value of i */
         //printf("\nThread %d flips %d\n", (int)(i % NROF_THREADS), (int)i);
@@ -110,10 +107,9 @@ static void control_threads(void) {
 
         /* join all threads after NROF_THREADS - 1 is created */
         if (!(i % NROF_THREADS)) {
-            for (pthread_t j = 0; j < NROF_THREADS; j++) {
+            for (uint16_t j = NROF_THREADS - 1; j != 0; j--) {
                 /* no return value needed */
                 pthread_join(flippers[j], NULL);
-                //printf("\nThread %d joined\n", (int)j);
             }
         }
     }
