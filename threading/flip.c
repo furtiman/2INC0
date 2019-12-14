edbdb4c14dcf86b7e5983092c425c64d2b29b424
Ivan Turasov <ivan@thethingsindustries.com>
2019-12-14 19:04:31 +0100

    Refactor flipping

--- threading/flip.c
+++ threading/flip.c
@@ -18,100 +18,98 @@
 #include <errno.h> // for perror()
 #include <pthread.h>
 
-#include "uint128.h"
 #include "flip.h"
 
+#define BUFFER_SIZE ((NROF_PIECES - 1) / 128) + 1
 
-void create_mask(uint128_t mask[], uint32_t flip_number);
-static void* flip_thread(void* flip_number);
+#define BITS_IN_UINT128 128
+
+#define BOARD_BIT_POSITION(buffer_cell, position_in_cell) (buffer_cell * BITS_IN_UINT128 + position_in_cell)
+
+// Create a bitmask where bit at position n is set
+#define BITMASK(n) (((uint128_t)1) << (n))
+// Check if bit n in v is set
+#define BIT_IS_SET(v, n) (((v)&BITMASK(n)) == BITMASK(n))
+// Set bit n in v
+#define BIT_SET(v, n) ((v) = (v) | BITMASK(n))
+// Clear bit n in v
+#define BIT_CLEAR(v, n) ((v) = (v) & ~BITMASK(n))
+
+static void *create_mask(void *flip_number);
 static void control_threads(void);
 
 static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
-int main(void) {
-    /* start threads, flip pieces, stop threads */
+int main(void)
+{
     control_threads();
 
-    for (uint16_t i = 0; i <= NROF_PIECES/128; i++) {
-        for (uint32_t j = 0; j < 127; j++) {
-            if (buffer[i] & 1) {
-                printf("%d", i*128 + j);
+    for (uint16_t i = 0; i < BUFFER_SIZE; ++i)
+    {
+        for (uint32_t j = 0; j < BITS_IN_UINT128; ++j)
+        {
+            if (BIT_IS_SET(buffer[i], j))
+            {
+                printf("%d", BOARD_BIT_POSITION(i, j));
                 printf("\n");
             }
-            buffer[i] >>= 1;
         }
     }
     return (0);
 }
 
-void create_mask(uint128_t mask[], uint32_t flip_number) {
-    const uint16_t buffer_size = NROF_PIECES/128 + 1;
-
-    /* inintialize mask array*/
-    for (uint16_t i = 0; i < buffer_size; i++) {
-        mask[i] = 0;
-    }
-
-    for (uint32_t i = NROF_PIECES; i > 0; i--) {
-        /* i counts backwards, piece_number counts upwards */
-        uint32_t piece_number = i - 1;
-        /* becomes 1 if piece_number i-1 should be flipperd, 0 if piece_number i-1 should not be flipped */
-        bool one_or_zero = !(piece_number % flip_number) && piece_number ? 1 : 0; 
-
-         for (uint16_t j = buffer_size - 1; j > 0; j--) {
-             /* shifts all bits left by 1 and puts the msb of arr[i - 1] in the lsb of arr[i] */
-             mask[j] = (mask[j] << 1) | ((mask[j - 1] & ((uint128_t)1 << 127)) >> 127);
-         }
-        /* shifts all bits left by 1 and puts a 1 or 0 at the lsb of the mask */
-
-        mask[0] = (mask[0] << 1) | one_or_zero;
-    }
-}
-
-static void* flip_thread(void* flip_number) {
+static void *create_mask(void *flip_number)
+{
     uint32_t *arg_number;
     uint32_t number;
 
-    arg_number = (uint32_t*)flip_number;
+    arg_number = (uint32_t *)flip_number;
     number = *arg_number;
     free(flip_number);
 
-    uint128_t mask[(NROF_PIECES/128) + 1];
-    create_mask(mask, number);
-
-    //printf("\nlocked\n");
-
-    pthread_mutex_lock(&mutex);
-    for (uint16_t i = 0; i <= NROF_PIECES/128; i++) {
-        /* buffer XOR mask works like flipping the pieces */
-        buffer[i] ^= mask[i];
+    for (uint16_t i = 0; i < BUFFER_SIZE; i++)
+    {
+        for (uint32_t j = 0; j < 128; ++j)
+        {
+            if (BOARD_BIT_POSITION(i, j) > NROF_PIECES)
+            {
+                break;
+            }
+            if (BOARD_BIT_POSITION(i, j) % number == 0)
+            {
+                pthread_mutex_lock(&mutex);
+                BIT_IS_SET(buffer[i], j) ? BIT_CLEAR(buffer[i], j) : BIT_SET(buffer[i], j);
+                pthread_mutex_unlock(&mutex);
+            }
+        }
     }
-    pthread_mutex_unlock(&mutex);
-
-    //printf("\nunlock\n");
-    return (NULL);
+    return NULL;
 }
 
-static void control_threads(void) {
-    uint32_t* flip_number;
+static void control_threads(void)
+{
+    static uint32_t *flip_number;
     /* array of threads */
-    pthread_t flippers[NROF_THREADS];
-
-    for (uint32_t i = 1; i <= NROF_PIECES; i++) {
+    static pthread_t flippers[NROF_THREADS];
+    static int active_threads = 0;
+    for (uint32_t i = 1; i <= NROF_PIECES; i++)
+    {
         flip_number = malloc(sizeof(uint32_t));
         *flip_number = i;
 
         /* created NROF_THREADS threads and gives them the current value of i */
-        //printf("\nThread %d flips %d\n", (int)(i % NROF_THREADS), (int)i);
-        pthread_create(&flippers[i % NROF_THREADS], NULL, flip_thread, flip_number);
+        pthread_create(&flippers[i % NROF_THREADS], NULL, create_mask, flip_number);
+        active_threads++;
 
         /* join all threads after NROF_THREADS - 1 is created */
-        if (!(i % NROF_THREADS)) {
-            for (uint16_t j = NROF_THREADS - 1; j != 0; j--) {
+        if ((active_threads % NROF_THREADS) == 0 || i == NROF_PIECES)
+        {
+            for (uint16_t j = 0; j < active_threads; ++j)
+            {
                 /* no return value needed */
                 pthread_join(flippers[j], NULL);
+                active_threads--;
             }
         }
     }
-    //printf("\ndone\n");
 }
--- threading/flip.h
+++ threading/flip.h
@@ -10,21 +10,21 @@
 #define _FLIP_H_
 
 #include "uint128.h"
- 
+
 /**
  * NROF_PIECES: size of the board; number of pieces to be flipped
  */
-#define NROF_PIECES			3000
+#define NROF_PIECES 3000
 
 /**
  * NROF_THREADS: number of threads that can be run in parallel
  * (value must be between 1 and ... (until you run out of system resources))
  */
-#define NROF_THREADS		10
+#define NROF_THREADS 100
 
 /**
  * buffer[]: datastructure of the pieces; each piece is represented by one bit
  */
-static uint128_t			buffer [(NROF_PIECES/128) + 1];
+static uint128_t buffer[NROF_PIECES / 128 + 1];
 
 #endif
